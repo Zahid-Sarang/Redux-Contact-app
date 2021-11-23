6a42b10907909feff8f44e7f229512c20f717d4a
zahid-soft <zahidsarang95@gmail.com>
2021-11-23 16:52:58 +0530

    display user data

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import './App.css';
-
+import React from 'react';
+import Contacts from './components/contacts/Contacts';
+import Navbar from './components/elements/Navbar';
+import './styles/App.scss';
+import store from "./store"
+import { Provider } from 'react-redux';
 function App() {
   return (
+    <Provider store={store}>
     <div className="App">
-  
+    <Navbar/>
+    <div className="conatiner">
+      <div className="py-3">
+        <Contacts/>
+      </div>
     </div>
+    </div>
+    </Provider>
   );
 }
 
---
+++ src/components/contacts/Contacts.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+
+const Contacts = () => {
+    const contacts = useSelector(state => state.contacts)
+    
+    return (
+       <div>
+           <table className="table shadow ">
+  <thead>
+    <tr>
+        <th scope="col">
+            <div className="custom-control custom-checkbox">
+                <input type="checkbox" className="custom-control-input"/>
+                <label className="custom-control-label"></label>
+            </div>
+        </th>
+        <th >Name</th>
+        <th >Phone</th>
+        <th >Email</th>
+    </tr>
+  </thead>
+  <tbody>
+  {
+      contacts.map(contact =>(
+        <tr>
+      <td>
+      <div className="custom-control custom-checkbox">
+                <input type="checkbox" className="custom-control-input"/>
+                <label className="custom-control-label"></label>
+            </div>
+      </td>
+      <td>{contact.name}</td>
+      <td>{contact.phone}</td>
+      <td>{contact.email}</td>
+    </tr>
+      ))
+  }
+    
+  </tbody>
+</table>
+       </div>
+    )
+}
+
+export default Contacts
