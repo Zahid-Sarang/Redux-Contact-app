ede41f1f238ebd5ffe7e139330a34b93d97eb34f
zahid-soft <zahidsarang95@gmail.com>
2021-11-23 20:31:49 +0530

    create contact form created

--- src/components/contacts/AddContact.js
+++ src/components/contacts/AddContact.js
@@ -1,9 +1,46 @@
-import React from 'react'
+import React,{ useState } from 'react'
 
 const AddContact = () => {
+    const [name,setName] = useState("");
+    const [email,setEmail] = useState("");
+    const [phone,setPhone] = useState("");
+
+    const createContact = (e) => {
+        e.preventDefault();
+        console.log(name,phone,email);
+    };
     return (
-        <div>
-            Create yur app
+        <div className="card border-0 shadow">
+        <div className="card-header">Add a Contact</div>
+        <div className="card-body">
+            <form onSubmit={(e) => createContact(e)}>
+                <div className="form-group">
+                    <input type="text" className="form-control"
+                        placeholder="Enter your Name"
+                        value={name}
+                        onChange={(e) => setName(e.target.value)}
+                    />
+                </div>
+                <div className="form-group">
+                    <input type="text" className="form-control"
+                        placeholder="Enter your Phone"
+                        value={phone}
+                        onChange={(e) => setPhone(e.target.value)}
+                    />
+                </div>
+                <div className="form-group">
+                    <input type="text" className="form-control"
+                        placeholder="Enter your Email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                    />
+                </div>
+                <button type="submit" className="btn btn-success">Success</button>
+            </form>
+         
+
+        </div>
+           
         </div>
     )
 }
