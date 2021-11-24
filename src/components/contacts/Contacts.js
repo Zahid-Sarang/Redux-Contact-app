469830435d3de3159ad17804a70b87c8ebb14f75
zahid-soft <zahidsarang95@gmail.com>
2021-11-24 12:00:18 +0530

    unique key prop error solve

--- src/components/contacts/Contacts.js
+++ src/components/contacts/Contacts.js
@@ -24,7 +24,7 @@ const Contacts = () => {
   </thead>
   <tbody>
   {
-      contacts.map(contact =><Contact contact={contact}/>  )
+      contacts.map(contact =><Contact contact={contact} key={contact.id}/>  )
   }
     
   </tbody>
